8055258434e9ec582a66597c22a99812c03ac724
Ivan Skoric <ivanskoric7@gmail.com>
2017-10-15 19:08:30 +0200

    Add AnimationSchedulers javadoc

--- vangogh/src/main/java/com/pspdfkit/vangogh/api/AnimationSchedulers.java
+++ vangogh/src/main/java/com/pspdfkit/vangogh/api/AnimationSchedulers.java
@@ -1,5 +1,6 @@
 package com.pspdfkit.vangogh.api;
 
+import com.pspdfkit.vangogh.rx.AnimationCompletable;
 import io.reactivex.Completable;
 
 /**
@@ -7,11 +8,21 @@ import io.reactivex.Completable;
  */
 public final class AnimationSchedulers {
 
-    public static Completable together(Completable... animations) {
+    /**
+     * Animates given animations at the same time.
+     * @param animations Animation to execute together.
+     * @return Completable finishing once the last animation is completed.
+     */
+    public static Completable together(AnimationCompletable... animations) {
         return Completable.mergeArray(animations);
     }
 
-    public static Completable sequentially(Completable... animations) {
+    /**
+     * Animates given animations one after another
+     * @param animations Animation to execute sequentially.
+     * @return Completable finishing once the last animation is completed.
+     */
+    public static Completable sequentially(AnimationCompletable... animations) {
         return Completable.concatArray(animations);
     }
 

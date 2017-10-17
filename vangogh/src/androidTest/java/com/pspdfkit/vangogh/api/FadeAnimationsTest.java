d80a7b1c990b85c7936b7013bf571a5536632c4c
Ivan Skoric <ivanskoric7@gmail.com>
2017-10-17 21:22:42 +0200

    Add fade out tests

--- vangogh/src/androidTest/java/com/pspdfkit/vangogh/api/FadeAnimationsTest.java
+++ vangogh/src/androidTest/java/com/pspdfkit/vangogh/api/FadeAnimationsTest.java
@@ -1,13 +1,8 @@
 package com.pspdfkit.vangogh.api;
 
-import android.app.Activity;
-import android.graphics.Color;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.annotation.UiThreadTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
-import com.pspdfkit.vangogh.R;
 import com.pspdfkit.vangogh.view.TestActivity;
 import io.reactivex.observers.TestObserver;
 import org.junit.Assert;
@@ -99,6 +94,40 @@ public class FadeAnimationsTest {
 
         // Assert view invisible.
         Assert.assertEquals(0f, view.getAlpha(), 0f);
+    }
+
+    @Test
+    public void testFadeOutWithDuration() throws InterruptedException {
+        final long duration = 1500L;
+
+        // Init test observer.
+        TestObserver o = new TestObserver();
+        o.assertNotComplete();
+
+        // Set view to visible and then fade out.
+        view.setAlpha(1f);
+        FadeAnimations.fadeOut(view, duration).subscribe(o);
+
+        // Wait one second and make sure it's not completed.
+        o.await(1, TimeUnit.SECONDS);
+        o.assertNotComplete();
+
+        // Wait another second and make sure it's completed.
+        o.await(1, TimeUnit.SECONDS);
+        o.assertComplete();
+
+        // Assert view invisible.
+        Assert.assertEquals(0f, view.getAlpha(), 0f);
+    }
+
+    @Test
+    public void testFadeOutWithInterpolator() {
+        // TODO Find a way to test interpolator.
+    }
+
+    @Test
+    public void testFadeOutWithDurationAndInterpolator() {
+        // TODO Find a way to test interpolator.
     }
 
 }

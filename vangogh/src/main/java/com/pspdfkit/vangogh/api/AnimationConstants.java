d7756115789b56f8f57430dfe22b0014e6cd9f5f
Ivan Skoric <ivanskoric7@gmail.com>
2017-10-30 22:44:45 +0100

    Make AnimationConstants class package-private

--- vangogh/src/main/java/com/pspdfkit/vangogh/api/AnimationConstants.java
+++ vangogh/src/main/java/com/pspdfkit/vangogh/api/AnimationConstants.java
@@ -6,24 +6,24 @@ import android.view.animation.Interpolator;
 /**
  * Helper class holding constants used in animations.
  */
-public class AnimationConstants {
+final class AnimationConstants {
 
     /** Full visibility alpha level. */
-    public static final float ALPHA_VISIBLE = 1f;
+    static final float ALPHA_VISIBLE = 1f;
 
     /** Full invisibility alpha level. */
-    public static final float ALPHA_INVISIBLE = 0f;
+    static final float ALPHA_INVISIBLE = 0f;
 
     /** Quick animation duration in ms. */
-    public static final long DURATION_QUICK = 300L;
+    static final long DURATION_QUICK = 300L;
 
     /** Slow animation duration in ms. */
-    public static final long DURATION_SLOW = 1500L;
+    static final long DURATION_SLOW = 1500L;
 
     /** Default animation duration in ms. */
-    public static final long DURATION_DEFAULT = 1000L;
+    static final long DURATION_DEFAULT = 1000L;
 
     /** Default interpolator used with animations if not specified otherwise. */
-    public static final Interpolator INTERPOLATOR = new AccelerateDecelerateInterpolator();
+    static final Interpolator INTERPOLATOR = new AccelerateDecelerateInterpolator();
 
 }

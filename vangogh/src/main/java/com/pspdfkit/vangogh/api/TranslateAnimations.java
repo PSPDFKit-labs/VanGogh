21d36e54dfffcd3709c56b5435ff3549d0eaac39
Ivan Skoric <ivanskoric7@gmail.com>
2017-10-30 22:53:24 +0100

    Add initial TranslateAnimations class

---
+++ vangogh/src/main/java/com/pspdfkit/vangogh/api/TranslateAnimations.java
@@ -0,0 +1,37 @@
+package com.pspdfkit.vangogh.api;
+
+import android.view.View;
+import android.view.animation.Interpolator;
+import com.pspdfkit.vangogh.rx.AnimationCompletable;
+import io.reactivex.annotations.NonNull;
+
+/**
+ * Class containing convenient animations affecting translation property of the view.
+ */
+public final class TranslateAnimations {
+
+    public static AnimationCompletable translateTo(@NonNull View view, float translationX, float translationY) {
+
+    }
+
+    public static AnimationCompletable translateTo(@NonNull View view, float translationX, float translationY, long durationMs) {
+
+    }
+
+    public static AnimationCompletable translateTo(@NonNull View view, float translationX, float translationY, Interpolator interpolator) {
+
+    }
+
+    public static AnimationCompletable translateTo(@NonNull View view, float translationX, float translationY, long durationMs, Interpolator interpolator) {
+
+    }
+
+    public static AnimationCompletable translateQuicklyTo(@NonNull View view, float translationX, float translationY) {
+
+    }
+
+    public static AnimationCompletable translateQuicklyTo(@NonNull View view, float translationX, float translationY, Interpolator interpolator) {
+
+    }
+
+}

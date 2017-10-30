001a51b04625726bfc2fdcb6a486f6199685d71c
Ivan Skoric <ivanskoric7@gmail.com>
2017-10-30 22:37:01 +0100

    Add javadoc to the Animation class

--- vangogh/src/main/java/com/pspdfkit/vangogh/base/Animation.java
+++ vangogh/src/main/java/com/pspdfkit/vangogh/base/Animation.java
@@ -106,141 +106,225 @@ public final class Animation {
         this.zBy = zBy;
     }
 
+    /**
+     * @return A weak reference to the view on which this animation is set to be applied.
+     */
     @NonNull
     public WeakReference<View> getView() {
         return view;
     }
 
+    /**
+     * @return Target alpha value of the animated view.
+     */
     @Nullable
     public Float getAlpha() {
         return alpha;
     }
 
+    /**
+     * @return Target alpha value difference of the animated view.
+     */
     @Nullable
     public Float getAlphaBy() {
         return alphaBy;
     }
 
+    /**
+     * @return Target rotation in degrees of the animated view.
+     */
     @Nullable
     public Float getRotation() {
         return rotation;
     }
 
+    /**
+     * @return Target rotation difference in degrees of the animated view.
+     */
     @Nullable
     public Float getRotationBy() {
         return rotationBy;
     }
 
+    /**
+     * @return Target rotation in degrees relative to the x-axis in degrees of the animated view.
+     */
     @Nullable
     public Float getRotationX() {
         return rotationX;
     }
 
+    /**
+     * @return Target rotation difference in degrees relative to the x-axis of the animated view.
+     */
     @Nullable
     public Float getRotationXBy() {
         return rotationXBy;
     }
 
+    /**
+     * @return Target rotation in degrees relative to the y-axis in degrees of the animated view.
+     */
     @Nullable
     public Float getRotationY() {
         return rotationY;
     }
 
+    /**
+     * @return Target rotation difference in degrees relative to the y-axis of the animated view.
+     */
     @Nullable
     public Float getRotationYBy() {
         return rotationYBy;
     }
 
+    /**
+     * @return Target x-axis scale of the animated view.
+     */
     @Nullable
     public Float getScaleX() {
         return scaleX;
     }
 
+    /**
+     * @return Target x-axis scale difference of the animated view.
+     */
     @Nullable
     public Float getScaleXBy() {
         return scaleXBy;
     }
 
+    /**
+     * @return Target y-axis scale of the animated view.
+     */
     @Nullable
     public Float getScaleY() {
         return scaleY;
     }
 
+    /**
+     * @return Target y-axis scale difference of the animated view.
+     */
     @Nullable
     public Float getScaleYBy() {
         return scaleYBy;
     }
 
+    /**
+     * @return Duration of the specified animation in milliseconds.
+     */
     @Nullable
     public Long getDuration() {
         return duration;
     }
 
+    /**
+     * @return Interpolator to use with the specified animation.
+     */
     @Nullable
     public Interpolator getInterpolator() {
         return interpolator;
     }
 
+    /**
+     * @return Initial animation delay in milliseconds.
+     */
     @Nullable
     public Long getStartDelay() {
         return startDelay;
     }
 
+    /**
+     * @return Target translation of the view along x-axis relative to the view's initial position.
+     */
     @Nullable
     public Float getTranslationX() {
         return translationX;
     }
 
+    /**
+     * @return Target translation difference of the view along x-axis relative to the view's initial position.
+     */
     @Nullable
     public Float getTranslationXBy() {
         return translationXBy;
     }
 
+    /**
+     * @return Target translation of the view along y-axis relative to the view's initial position.
+     */
     @Nullable
     public Float getTranslationY() {
         return translationY;
     }
 
+    /**
+     * @return Target translation of the view along y-axis relative to the view's initial position.
+     */
     @Nullable
     public Float getTranslationYBy() {
         return translationYBy;
     }
 
+    /**
+     * @return Target translation of the view along z-axis relative to the view's initial position.
+     */
     @Nullable
     public Float getTranslationZ() {
         return translationZ;
     }
 
+    /**
+     * @return Target translation of the view along z-axis relative to the view's initial position.
+     */
     @Nullable
     public Float getTranslationZBy() {
         return translationZBy;
     }
 
+    /**
+     * @return Target x property of the animated view.
+     */
     @Nullable
     public Float getX() {
         return x;
     }
 
+    /**
+     * @return Target x property difference of the animated view.
+     */
     @Nullable
     public Float getXBy() {
         return xBy;
     }
 
+    /**
+     * @return Target y property of the animated view.
+     */
     @Nullable
     public Float getY() {
         return y;
     }
 
+    /**
+     * @return Target y property difference of the animated view.
+     */
     @Nullable
     public Float getYBy() {
         return yBy;
     }
 
+    /**
+     * @return Target z property of the animated view.
+     */
     @Nullable
     public Float getZ() {
         return z;
     }
 
+    /**
+     * @return Target z property difference of the animated view.
+     */
     @Nullable
     public Float getZBy() {
         return zBy;

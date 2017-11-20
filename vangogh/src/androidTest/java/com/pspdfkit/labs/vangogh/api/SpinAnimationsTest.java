b22c9b0e254a9527a2bb1ec8cc7ff8597347969b
Ivan Skoric <ivanskoric7@gmail.com>
2017-11-21 00:42:30 +0100

    Add tests structure for SpinAnimations

---
+++ vangogh/src/androidTest/java/com/pspdfkit/labs/vangogh/api/SpinAnimationsTest.java
@@ -0,0 +1,82 @@
+package com.pspdfkit.labs.vangogh.api;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SpinAnimationsTest {
+
+    public void spinRight() {
+
+    }
+
+    public void spinRightWithCycles() {
+
+    }
+
+    public void spinRightWithCyclesAndDuration() {
+
+    }
+
+    public void spinRightWithCyclesAndInterpolator() {
+
+    }
+
+    public void spinRightWithCyclesDurationAndInterpolator() {
+
+    }
+
+    public void spinRightQuickly() {
+
+    }
+
+    public void spinRightQuicklyWithInterpolator() {
+
+    }
+
+    public void spinRightSlowly() {
+
+    }
+
+    public void spinRightSlowlyWithInterpolator() {
+
+    }
+
+    public void spinLeft() {
+
+    }
+
+    public void spinLeftWithCycles() {
+
+    }
+
+    public void spinLeftWithCyclesAndDuration() {
+
+    }
+
+    public void spinLeftWithCyclesAndInterpolator() {
+
+    }
+
+    public void spinLeftWithCyclesDurationAndInterpolator() {
+
+    }
+
+    public void spinLeftQuickly() {
+
+    }
+
+    public void spinLeftQuicklyWithInterpolator() {
+
+    }
+
+    public void spinLeftSlowly() {
+
+    }
+
+    public void spinLeftSlowlyWithInterpolator() {
+
+    }
+
+}

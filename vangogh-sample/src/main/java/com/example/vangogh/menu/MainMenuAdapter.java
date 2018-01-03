fc40f5ece2957115c602a2f513df0d090b5e129b
Ivan Skoric <ivanskoric7@gmail.com>
2018-01-03 08:31:25 +0100

    Create list adapter

---
+++ vangogh-sample/src/main/java/com/example/vangogh/menu/MainMenuAdapter.java
@@ -0,0 +1,40 @@
+package com.example.vangogh.menu;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.vangogh.R;
+
+import java.util.ArrayList;
+
+public class MainMenuAdapter extends ArrayAdapter<MainMenuItem> {
+
+    public MainMenuAdapter(@NonNull Context context, ArrayList<MainMenuItem> items) {
+        super(context, 0, items);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        MainMenuItem item = getItem(position);
+        if (item == null) return super.getView(position, convertView, parent);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.layout_main_menu_item, parent, false);
+        }
+
+        TextView itemTitle = convertView.findViewById(R.id.itemTitle);
+        TextView itemDesc = convertView.findViewById(R.id.itemDesc);
+
+        itemTitle.setText(item.getTitle());
+        itemDesc.setText(item.getDesc());
+
+        return convertView;
+    }
+}

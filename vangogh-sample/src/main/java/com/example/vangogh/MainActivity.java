365ddd2afea4cddc92eaa97f8a2c7b9eefac8e5f
Ivan Skoric <ivanskoric7@gmail.com>
2018-01-16 11:04:57 +0100

    Add initial sample activity (empty)

--- vangogh-sample/src/main/java/com/example/vangogh/MainActivity.java
+++ vangogh-sample/src/main/java/com/example/vangogh/MainActivity.java
@@ -8,8 +8,10 @@ import android.widget.ListView;
 
 import com.example.vangogh.menu.MainMenuAdapter;
 import com.example.vangogh.menu.MainMenuItem;
+import com.example.vangogh.samples.SingleAnimation;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements MainMenuAdapter.OnMainMenuItemClickListener {
 
@@ -22,13 +24,21 @@ public class MainActivity extends AppCompatActivity implements MainMenuAdapter.O
 
         mainMenuList = findViewById(R.id.mainMenu);
 
-        MainMenuAdapter adapter = new MainMenuAdapter(this, new ArrayList<MainMenuItem>());
+        MainMenuAdapter adapter = new MainMenuAdapter(this, getMainMenuItems());
+        adapter.setOnMainMenuItemClickListener(this);
         mainMenuList.setAdapter(adapter);
     }
 
     @Override
     public void onMainMenuItemClicked(@NonNull MainMenuItem item) {
         startActivity(new Intent(this, item.getActivity().getClass()));
+    }
+
+    @NonNull
+    public List<MainMenuItem> getMainMenuItems() {
+        List<MainMenuItem> items = new ArrayList<>();
+        items.add(new MainMenuItem("Single animation", "Showcases how single animations are executed", new SingleAnimation()));
+        return items;
     }
 
 }
